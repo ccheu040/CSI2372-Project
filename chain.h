2901fd37502a8f21e90db7c578e25064e7dd3b0b
ZhengguoWang <15122970201@163.com>
2016-11-23 23:33:21 -0500

    Add files via upload

--- chain.h
+++ chain.h
@@ -5,30 +5,41 @@ class CardFactory;
 
 #include <iostream>
 #include <vector>
+#include "card_hierarchy.h"
 
 using std::vector;
 using std::istream;
 using std::ostream;
 
+class Chain_Base {
+		string _dChainType;
+public:
+	string getChainType() {
+		return _dChainType;
+	}
+};
+
+
 template <class T>
-class Chain {
+class Chain::public Chain_Base {
 	vector<T*> chain;
 
 public:
 	Chain(istream& in, CardFactory* factory) const;
 	int sell() const;
 	Chain<T>& operator+=(Card* card);
-	friend ostream& operator<<(ostream& out, const Chain<T>& oChain);
+	friend ostream& operator << (ostream& out, const Chain<T>& oChain);
+	friend istream& operator >> (istream& in, const Chain<T>& oChain);
 };
 
 template <class T>
 Chain<T>::Chain(istream& in, CardFactory* factory) const {
-
+	
 }
 
 template <class T>
 int Chain<T>::sell() const {
-	return T.getCoinsPerCard(chain.size());
+	return T[0]->getCoinsPerCard(chain.size());
 }
 
 template <class T>
@@ -45,5 +56,17 @@ Chain<T>& Chain<T>::operator+=(Card* card) {
 
 template <class T>
 ostream& operator<<(ostream& out, const Chain<T>& oChain) {
+	out << oChain.getChainType()<<"  ";
+	for (int i = 0; i < chain.size(); i++) {
+		oChain[i]->print(out);
+		out << " ";
+	}
+	out << std::endl;
+	return out;
+}
 
+
+template <class T>
+istream& operator >> (istream& in, const Chain<T>& oChain) {
+	oChain += (in >> chain._dChainType);
 }
