5fff213bead4999e68cc7fbdb90ad7dd31578cf2
ccheu040 <ccheu040@uottawa.ca>
2016-11-25 11:01:06 -0500

    Modify operator[] in hand.cpp

--- hand.cpp
+++ hand.cpp
@@ -22,15 +22,18 @@ Hand& Hand::operator+=(Card* card) {
 	return *this;
 }
 
-Card* Hand::operator[](const int i) {
+Card* Hand::operator[](const int index) {
 	vector<Card*> vHand;
 	while (!hand.empty()) {
 		vHand.push_back(hand.front());
 		hand.pop();
 	}
-	Card* card = vHand[i];
-	vHand.erase(vHand.begin() + i);
-
+	Card* card = vHand[index];
+	vHand.erase(vHand.begin() + index);
+	for (int i = 0; i < vHand.size(); ++i) {
+		hand.push(vHand[i]);
+	}
+	return card;
 }
 
 ostream& operator<<(ostream& out, const Hand& hand) {
