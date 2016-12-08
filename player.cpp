92bfbc806485823981962e7006d54a916f24ddb4
ccheu040 <ccheu040@uottawa.ca>
2016-12-07 22:01:22 -0500

    Minor changes to Player

--- player.cpp
+++ player.cpp
@@ -1,61 +1,54 @@
-#include <iostream>
 #include "player.h"
-#include <exception>
-#include <string>
+
 using std::string;
+
+Player::Player(string& name) : name(name), coins(0), maxNumChain(2) {}
+
 Player::Player(istream& in, CardFactory* factory){
 
 }
 
-Player::Player() {
-
-}
-
-
-
-Player::Player(string& name) {
-	name = name;
-	coin = 0;
-	numChain = 0;
-	maxNumChain = 2;
-}
 string Player::getName() const {
 	return name;
 }
+
 int Player::getNumCoins() const {
-	return coin;
+	return coins;
 }
+
 Player& Player::operator+=(int i) {
-	this->coin += i;
+	coins += i;
 	return *this;
 }
+
 int Player::getMaxNumChains() const {
 	return maxNumChain;
 }
+
 int Player::getNumChains() const {
-	return numChain;
+	return _dChain.size();
 }
+
 Chain_Base& Player::operator[](int i) {
 	return *_dChain[i];
 }
-void Player::buyThirdChain() const {
-	
-		if (maxNumChain == 3) {
-			std::cout<< "Already have three chains" ;
-		}
-		else if (coin < 2) {
-			std::cout << "Does not have enough coin";;
-		}
-		else {
-			maxNumChain = 3;
-			coin = coin - 2;
-		}
+
+void Player::buyThirdChain() {
+	if (maxNumChain == 3) {
+		std::cout << "You already have three chains" << std::endl;
 	}
-	
+	else if (coins < 2) {
+		std::cout << "You do not have enough coins" << std::endl;
+	}
+	else {
+		maxNumChain = 3;
+		coins = coins - 2;
+	}
 }
+	
 void Player::printHand(ostream& out, bool p) {
 	if (p) {
-		out << ;
+		out << hand;
 	}
 	else {
 		std::cout << hand->top() << std::endl;
--- player.h
+++ player.h
@@ -1,24 +1,27 @@
 #ifndef _Player_h_
 #define _Player_h_
+
 #include "chain.h"
 #include "hand.h"
 #include <iostream>
 #include <string>
 #include <vector>
+
 using std::istream;
-using std::string;
 using std::ostream;
+using std::string;
+
 class Player {
 	string name;
-	int coin;
-	int numChain;
+	int coins;
 	int maxNumChain;
 	vector<Chain_Base*> _dChain;
 	Hand* hand;
+
 public:
-	Player(istream& in, CardFactory* factory);
-	Player();
+	Player() = default;
 	Player(string& name);
+	Player(istream& in, CardFactory* factory);
 	string getName() const;
 	int getNumCoins() const;
 	Player& operator+=(int num);
@@ -26,10 +29,10 @@ public:
 	int getNumChains() const;
 
 	Chain_Base& operator[](int i);
-	void buyThirdChain() const;
+	void buyThirdChain();
 	void printHand(ostream& out, bool);
 
-		friend ostream& operator<<(ostream& out, Player& player) {
+	friend ostream& operator<<(ostream& out, Player& player) {
 		out << player.getName() << "  " << player.getNumCoins() << " coins" << std::endl;
 		for (int i = 0; i < _dChain.size(); i++) {
 			out << *_dChain[i];
